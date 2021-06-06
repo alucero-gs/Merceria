272ad68c2329d9be1901cf8940313df4485620ce
maxeziowolf <63032627+maxeziowolf@users.noreply.github.com>
2021-06-06 14:31:22 -0500

    VentanaHomeCompras
    Se realizo unicamente el avance de esta pantalla y la funcionalidad de la misma

---
+++ MerceriaGit/Controllers/HomeComprasController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MerceriaGit.Models;
+
+namespace MerceriaGit.Controllers
+{
+    public class HomeComprasController : Controller
+    {
+        // GET: HomeCompras
+        public ActionResult Index()
+        {
+            contextTienda db = new contextTienda();
+
+            List<Productos> productosPocoStock = null;
+            var query = from p in db.Productos
+                        where p.Existencia <=10 
+                        select p;
+            productosPocoStock = query.ToList();
+
+            List<ImagenesProducto> imagenesProductos = db.ImagenesProducto.ToList();
+
+            ViewBag.productosPocoStock = productosPocoStock;
+            ViewBag.imagenesProductos = imagenesProductos;
+            return View();
+        }
+    }
+}
