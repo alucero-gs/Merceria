bf09fa08bf0604ed599987600de15526f16941e8
maxeziowolf <63032627+maxeziowolf@users.noreply.github.com>
2021-06-07 00:38:14 -0500

    Pantalla home de envios
    Parte funcional de esta pantalla y con agregados de la misma

---
+++ MerceriaGit/Controllers/EnviosHomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MerceriaGit.Models;
+
+namespace MerceriaGit.Controllers
+{
+    public class EnviosHomeController : Controller
+    {
+        // GET: EnviosHome
+        public ActionResult Index()
+        {
+            contextTienda db = new contextTienda();
+
+            List<Envios> listaEnvios = db.Envios.ToList();
+
+            ViewBag.listaEnvios = listaEnvios;
+
+            return View();
+        }
+    }
+}
